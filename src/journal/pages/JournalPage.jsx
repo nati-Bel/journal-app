063137df7bf6e2a95f5d62165dd28c3174966d91
natalia.belzarova <natalia.belzarova@gmail.com>
2023-11-13 14:11:11 +0100

    journal page layout ok

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,15 @@
 
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-      
-    </>
+    <JournalLayout>
+      <Typography sx={{ ml:'240px'}}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur veniam
+        esse deserunt dignissimos dolore! Iste modi repellendus quam,
+       
+      </Typography>
+    </JournalLayout>
   );
 }
