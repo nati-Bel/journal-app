c4891b2a42742e47ddfb3f882bcead50212f881f
natalia.belzarova <natalia.belzarova@gmail.com>
2023-11-14 16:34:50 +0100

    NothingSelectedView and NoteView design ok

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -6,19 +6,14 @@ import { SideBar, NavBar } from "../components";
 const drawerWidth = 240;
 export const JournalLayout = ({ children }) => {
   return (
-    
-      <Box sx={{ display: 'flex' }}>
-        <NavBar drawerWidth={drawerWidth}></NavBar>
-        <SideBar drawerWidth={drawerWidth}></SideBar>
+    <Box sx={{ display: "flex" }}>
+      <NavBar drawerWidth={drawerWidth}></NavBar>
+      <SideBar drawerWidth={drawerWidth}></SideBar>
 
-        <Box 
-          component="main" 
-          sx={{ flexGrow: 1, p: 3 }}
-        >
-            <Toolbar/>
-            {children}
-        </Box>
+      <Box component="main" sx={{ flexGrow: 1, p: 2, ml: "240px" }}>
+        <Toolbar />
+        {children}
       </Box>
-   
+    </Box>
   );
 }
