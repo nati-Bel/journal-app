b3874592da3d016f4b3a4367ca46c249e34a122e
natalia.belzarova <natalia.belzarova@gmail.com>
2023-11-15 11:01:30 +0100

    ImageGallery and floating btn added, JournalPage design ready

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,26 @@
 
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
-import { NoteView } from "../views";
+import { NoteView, NothingSelectedView } from "../views";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      {/* <NothingSelectedView></NothingSelectedView> */}
-      <NoteView></NoteView>
+      <NothingSelectedView />
+      {/* <NoteView></NoteView> */}
+      <IconButton
+        size="large"
+        sx={{color:'white',
+        backgroundColor:'error.main',
+        ':hover':{backgroundColor:'error.main', opacity: 0.9},
+        position:'fixed',
+        right:50,
+        bottom:50}}>
+          <AddOutlined />
+
+      </IconButton>
        
     
     </JournalLayout>
