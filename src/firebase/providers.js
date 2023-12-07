d5b91dff50d089c4f5f48ac848f8b586a01d8a7c
natalia.belzarova <natalia.belzarova@gmail.com>
2023-12-07 20:59:22 +0100

    login ok, display error message

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth'
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth'
 import { FirebaseAuth } from './config';
 
 const googleProvider = new GoogleAuthProvider();
@@ -37,16 +37,29 @@ export const registerUserWithEmailPassword = async ({ email, password, displayNa
         const { uid, photoURL } = resp.user;
 
         await updateProfile( FirebaseAuth.currentUser, { displayName});
-        
+
         return {
             ok:true,
             uid, photoURL, email, displayName
         }
-
-
 
     } catch (error) {
         console.log(error);
         return {ok:false, errorMessage : error.message}
     }
 }
+
+export const loginWithEmailPassword = async ({ email, password }) => {
+  
+    try {
+
+        const resp = await signInWithEmailAndPassword(FirebaseAuth, email, password);
+        const { uid, photoURL, displayName } = resp.user;
+
+        return { ok: true, uid, photoURL, displayName,
+    };
+
+  } catch (error) {
+        return { ok: false, errorMessage: error.message };
+  }
+};
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { registerUserWithEmailPassword, signInWithGoogle } from '../../firebase/providers';
+import { loginWithEmailPassword, registerUserWithEmailPassword, signInWithGoogle } from '../../firebase/providers';
 import {checkingCredentials, logout, login } from './authSlice'
 
 
@@ -30,6 +30,18 @@ export const startCreatingUserWithEmailPassword = ({email, password, displayName
         if (!ok) return dispatch( logout({errorMessage}))
 
         dispatch( login({uid, displayName, photoURL, email}))
+    }
+
+}
+
+export const startLoginWithEmailPassword = ({email, password }) => {
+    return async( dispatch ) => {
+        
+        dispatch(checkingCredentials());
+
+        const result = await loginWithEmailPassword({ email, password })
+        if ( !result.ok ) return dispatch( logout(result));
+        dispatch(login(result)); 
     }
 
 }
