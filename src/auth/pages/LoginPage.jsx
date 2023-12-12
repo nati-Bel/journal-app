48dd106dac1a32b2cdd64db6877e3102bf089313
natalia.belzarova <natalia.belzarova@gmail.com>
2023-12-12 14:59:49 +0100

    Create CheckingAuth and loader

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -14,8 +14,8 @@ export const LoginPage = () => {
   const dispatch = useDispatch();
 
   const {email, password, onInputChange} = useForm({
-    email: 'nati@nati.com',
-    password: '12345'
+    email: '',
+    password: ''
   });
 
   const isAuthenticating = useMemo( () => status ==='checking', [status])
