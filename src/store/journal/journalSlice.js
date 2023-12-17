8fd90a5968787189c71a87182c9d0559f71e9d07
natalia.belzarova <natalia.belzarova@gmail.com>
2023-12-17 09:47:51 +0100

    set active note, disable button when saving

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const journalSlice = createSlice({
   name: "journal",
   initialState: {
-    isSaving: true,
+    isSaving: false,
     messageSaved: "",
     notes: [],
     active: null,
@@ -16,8 +16,16 @@ export const journalSlice = createSlice({
     //}
   },
   reducers: {
-    addNewEmptyNote: (state, action) => {},
-    setActiveNote: (state, action) => {},
+    addNewEmptyNote: (state, action) => {
+      state.notes.push( action.payload);
+      state.isSaving = false;
+    },
+    setActiveNote: (state, action) => {
+      state.active = action.payload;
+    },
+    savingNewNote: (state) => {
+      state.isSaving = true;
+    },
     setNotes: (state, action) => {},
     setSaving: (state) => {},
     updateNote: (state, action) => {},
@@ -33,4 +41,5 @@ export const {
   setSaving,
   updateNote,
   deleteNoteById,
+  savingNewNote,
 } = journalSlice.actions;
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,8 +1,11 @@
-import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { collection, doc, setDoc, } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
+import { addNewEmptyNote, savingNewNote, setActiveNote } from "./journalSlice";
 
 export const startNewNote = () => {
     return async( dispatch, getState ) =>{
+
+        dispatch(savingNewNote());
 
         const{ uid } = getState().auth;
 
@@ -11,15 +14,16 @@ export const startNewNote = () => {
             body:'',
             date: new Date().getTime(),
 
-        }
-
+        }        
         const newDoc = doc(collection(FirebaseDB, `${ uid }/journal/notes`));
-        const setDocResp = await setDoc( newDoc, newNote)
+        await setDoc(newDoc, newNote);
+        
 
-        console.log(newDoc, setDocResp)
+        newNote.id = newDoc.id
 
-        //dispatch
-        //dispatch( newNote)
-        //dispatch( activateNote)
+        
+        dispatch( addNewEmptyNote( newNote ) );
+        dispatch( setActiveNote( newNote ) );
+        
     }
 }
