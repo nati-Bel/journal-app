bdca4cca8a3fee18da7a31d5d252e7b89e3f2350
natalia.belzarova <natalia.belzarova@gmail.com>
2023-12-18 17:24:29 +0100

    set a note active

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,18 +1,28 @@
 import { TurnedInNot } from "@mui/icons-material";
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { setActiveNote } from "../../store/journal/journalSlice";
 
 
-export default function SideBarItem({ title= '', body, id }) {
+export default function SideBarItem({ title= '', body, id, date, imageUrls=[] }) {
+    
+    const dispatch = useDispatch()
 
     const newTitle = useMemo( () => {
         return title.length > 17 
         ? title.substring(0,17) + '...'
         : title
     }, [title])
+
+    const onClickNote = () => {
+        dispatch(setActiveNote({ title, body, id, date, imageUrls }))
+
+    }
   return (
     <ListItem  disablePadding>
-      <ListItemButton>
+      <ListItemButton 
+        onClick={ onClickNote }>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
